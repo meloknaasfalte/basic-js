c3dbd02f60e613599e26c1a8dab2ce9ea2f4418d
meloknaasfalte <you@example.com>
2022-05-02 21:03:27 +0300

    upd

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,8 +14,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(matrix) {
- 
+ function countCats(arr) {
+
+  let count = 0;
+
+  arr.forEach(element => {
+    element.forEach(el => {
+      if (el === '^^') {
+        count++ ;
+      }
+    })
+  })
+  return count;
 }
 
 module.exports = {
