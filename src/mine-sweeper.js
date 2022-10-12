fe74bff2861fe7c09f026bf847afbdb9509fe556
meloknaasfalte <you@example.com>
2022-10-12 23:14:08 +0300

    test

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -28,7 +28,8 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 // }
 function minesweeper(matrix) {
-
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
 }
 
 console.log(minesweeper(matrix));
